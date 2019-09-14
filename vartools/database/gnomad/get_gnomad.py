e3324728b7b7e478f00302c7268aaa6eea4e955b
James Allen <allen.james.allen@gmail.com>
2019-09-13 23:58:00 -0400

    fixed gnomad array out of index error due to synonymous variants not containing an original and variant amino acid consequence

--- vartools/database/gnomad/get_gnomad.py
+++ vartools/database/gnomad/get_gnomad.py
@@ -81,41 +81,32 @@ def get_gnomad_data(chrom, version, exomes = True):
 
 import hail as hl
 
-def process_gnomad_data(datapath, chromosome, transcript_list, synonymous = True):
+def process_gnomad_data(datapath, chromosome, transcript_list, exomes = True, synonymous = True):
     """
     Uses hail to process the gnomAD dataset 
     """
     hl.init()
     # this try-except block makes sure the program won't spend time 
     # writing the table to disk if it already exists from a previous loop
-    try:
-        mt = hl.import_vcf(datapath).write('temp_matrix_table_' + chromosome + '.mt',
-                                           overwrite = False)
-    except:
-        #it already exists, so just read it.
-        pass
-    mt = hl.read_matrix_table('temp_matrix_table_' + chromosome + '.mt')
-    #mt = hl.import_vcf(datapath)
+    #try:
+    #    mt = hl.import_vcf(datapath)
+    #except:
+    #    #it already exists, so just read it.
+    #    pass
+    mt = hl.import_vcf(datapath)
     # first filter down to the right number of transcripts
     transcripts, intervals = zip(*transcript_list)
-    print(transcripts)
-    print(intervals)
     transcripts = hl.literal(list(transcripts))
-    print(transcripts)
     mt = hl.filter_intervals(mt, [hl.parse_locus_interval(x,
                              reference_genome = 'GRCh37') for x in intervals]
                             )
-    mt.show(5)
+    mt = mt.filter_rows(mt.filters == hl.empty_set('str'))
     mt = mt.explode_rows(mt.info.vep)
-    # get the right tranhi oscript
+    # get the right transcript
     mt = mt.annotate_rows(vep = mt.info.vep.split('\|'))
-    mt.vep.show(5)
     mt = mt.annotate_rows(enst = mt.vep[6])
-    mt.enst.show(5)
     mt = mt.filter_rows(transcripts.contains(mt.enst))
-    mt.vep.show(5)
     mt = mt.annotate_rows(vartype = mt.vep[1].split('&'))
-    mt.vartype.show(5)
     mt = mt.explode_rows(mt.vartype)
     vartype_list = hl.literal(['frameshift_variant','inframe_deletion',
                                'inframe_insertion','missense_variant',
@@ -123,28 +114,43 @@ def process_gnomad_data(datapath, chromosome, transcript_list, synonymous = True
     if synonymous:
         vartype_list = vartype_list.extend(['synonymous_variant'])
     mt = mt.filter_rows(vartype_list.contains(mt.vartype))
-    mt.show(5)
-    mt = mt.annotate_rows(aanum = mt.vep[14])
-    mt.show(5)
-    mt = mt.annotate_rows(orig_aa = mt.vep[15].split('/')[0])
-    mt = mt.annotate_rows(var_aa = mt.vep[15].split('/')[1])
-    mt = mt.annotate_rows(cDNA_conseq = mt.vep[10])
-    mt = mt.annotate_rows(Protein_conseq = mt.vep[11])
-    mt.orig_aa.show(5)
-    mt.describe()
+    mt = mt.annotate_rows(aa_num = mt.vep[14])
+    mt = mt.annotate_rows(aa_change = mt.vep[15])
+    #mt = mt.annotate_rows(orig_aa = mt.vep[15].split('/')[0])
+    #mt = mt.annotate_rows(var_aa = mt.vep[15].split('/')[1])
+    #mt.filter_rows(mt.vartype == "synonymous_variant").var_aa = None
+    mt = mt.annotate_rows(transcript_consequence = mt.vep[10])
+    mt = mt.annotate_rows(protein_consequence = mt.vep[11])
+    ht = mt.select_rows(mt.qual,
+                        mt.filters, mt.vartype,
+                        mt.transcript_consequence, mt.protein_consequence,
+                        mt.aa_num, mt.aa_change,
+                        mt.info.FS, mt.info.MQRankSum, mt.info.InbreedingCoeff,
+                        mt.info.ReadPosRankSum, mt.info.VQSLOD, mt.info.QD,
+                        mt.info.DP, mt.info.BaseQRankSum, mt.info.MQ, mt.info.ClippingRankSum,
+                        mt.info.rf_tp_probability, mt.info.pab_max,
+                        mt.info.AC, mt.info.AN,
+                        mt.info.non_neuro_AC, mt.info.non_neuro_AN,
+                        mt.info.non_topmed_AC, mt.info.non_topmed_AN,
+                        mt.info.controls_AC, mt.info.controls_AN
+                        ).make_table()
+    """
     ht = mt.select_rows(mt.info.variant_type, mt.qual, mt.filters,
                         mt.enst, mt.vartype, mt.cDNA_conseq,
                         mt.Protein_conseq, mt.orig_aa, mt.aanum,
                         mt.var_aa, mt.info.AC, mt.info.AN,
                         mt.info.non_neuro_AC, mt.info.non_neuro_AN,
-                        mt.info.non_cancer_AC, mt.info.non_cancer_AN,
                         mt.info.non_topmed_AC, mt.info.non_topmed_AN,
                         mt.info.controls_AC, mt.info.controls_AN
                         ).make_table()
-    ht.show(5)
-    filename = 'gnomad_chr' + chromosome + '_processed.tsv'
-    #ht.export(filename)
-    os.remove('temp_matrix_table_' + chromosome + '.mt')
+    """
+    if exomes:
+        ome = 'exomes'
+    else:
+        ome = 'genomes'
+    filename = 'gnomad_' + ome + '_chr' + chromosome + '_processed.tsv'
+    ht.export(filename)
+    #os.remove('temp_matrix_table_' + chromosome + '.mt')
     hl.stop()
     return None
 
@@ -155,8 +161,8 @@ def build_gnomAD_FromTranscriptList(transcript_list_file, gmd_version):
     for chrom, transcript_list in transcript_dict.items():
         for a in [False, True]:
             #datapath = get_gnomad_data(chrom, version = gmd_version, exomes = a)
-            datapath = 'gnomad.exomes.r2.1.1.sites.9.vcf.bgz'
-            process_gnomad_data(datapath, chrom, transcript_list)
+            datapath = 'gnomad_2.1.1_chr9.vcf.bgz'
+            process_gnomad_data(datapath, chrom, transcript_list, exomes = a)
     return None
 
 if __name__ == "__main__":
